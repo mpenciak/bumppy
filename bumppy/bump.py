c6dc8ce879737c6cd0b60c6ed0134a47a210a223
Matej Penciak <matej.penciak@gmail.com>
2023-03-28 15:16:27 -0400

    delete repetition

--- bumppy/bump.py
+++ bumppy/bump.py
@@ -12,16 +12,3 @@ def get_bump_project():
     with open(CURR_DIR/"bump.toml", 'rb') as file:
         bump_data = tomli.load(file)
     return BumpProject.from_toml(bump_data)
-
-def get_dependencies_from_lakefile(lakefile_str: str):
-    dependency_re_str = r'require\s+(.*?)\s+from\s+git\s+"(.*?)"\s+@\s+"(.*?)"'
-    dependency_re = re.compile(dependency_re_str)
-    dep_data = dependency_re.findall(lakefile_str)
-    answer = {}
-    for dep in dep_data:
-        answer[dep[0]] = {
-            "url": dep[1],
-            "sha": dep[2]
-        }
-    return answer
-
